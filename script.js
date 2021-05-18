5487f07943ee119a3af1b4234a681ab857674fb6
Derek Jackson <d4jackson84@gmail.com>
2021-05-17 23:15:07 -0400

    styling update

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 const section = document.querySelectorAll("section");
-
 const right = document.querySelector(".right");
-
-
-
-
 right.addEventListener("scroll", (event) => {
     console.log("scrolled");
 })
 
+// left side totals & cart
+let subtotal = document.querySelector(".subtotal").innerText;
+let tax = document.querySelector(".tax").innerText;
+let total = document.querySelector(".total");
+let viewCart = document.querySelector(".viewCart");
 
+viewCart.addEventListener("click", event => {
+    event.preventDefault();
+    document.createElement("div").setAttribute("class", "cartCard");
+    console.log("clicked");
+    console.log(queryselector(".cartCard"))
+});
 
 
 
