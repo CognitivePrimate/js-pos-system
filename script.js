2d2327e3e69d073c72344a4f2ffdcaf59485c7e2
Derek Jackson <d4jackson84@gmail.com>
2021-05-20 16:23:50 -0400

    more js wiring and animations, now with more DOM

--- script.js
+++ script.js
@@ -12,39 +12,35 @@ let viewCart = document.querySelector(".view-cart");
 let addToCart = document.querySelectorAll(".add-to-cart-button");
 let newTotal = 0;
 
+// TEST - SAME AS ABOVE BUT TRYING TO ADD TOTALS TO CART SIMULTANEOUSLY
+
 // add to cart process
 let right = document.querySelector(".right");
 let productCards = document.querySelectorAll(".products-card");
 
 // add to cart visual representation variables
 let cart = [];
-// IMPORTANT FOR CLASS
-const shoppingCart = document.querySelector(".hidden");
-// IMPORTANT FOR CLASS
+let cartSubTotal = document.querySelector(".cart-sub-total");
+let cartTax = document.querySelector(".cart-tax");
+let cartTotal = document.querySelector(".cart-total");
 let cartWrapper = document.querySelector(".cart-wrapper");
 let cartNameHolder = document.querySelector(".cart-product-name");
 let cartQuantityHolder = document. querySelector(".cart-change-quanity");
 let cartProductPriceHolder = document.querySelector(".cart-product-price");
+// IMPORTANT FOR CLASS
+const shoppingCart = document.querySelector(".hidden");
+// IMPORTANT FOR CLASS
 
-// add to cart visual representation wiring
-// create p tag to insert item.name et al
+
+
+// toggles shopping cart view
 viewCart.addEventListener("click", (event) => {
     event.preventDefault();
-    for (item of cart){
-        let cartItemNameDisplay = document.createElement("p");
-        cartItemNameDisplay.innerText = item.name;
-        cartNameHolder.appendChild(cartItemNameDisplay);
-        
-        let cartQuantityHolder = "something to fix";
-
-        let cartPriceDisplay = document.createElement("p");
-        cartPriceDisplay.innerText = item.price;
-        cartProductPriceHolder.appendChild(cartPriceDisplay);
-    }
-    // let cartToggle = shoppingCart.toggle(".hidden");
     let cartToggle = () => shoppingCart.classList.toggle("shopping-cart");
     cartToggle();
 });
+
+
 
 
 
@@ -53,27 +49,48 @@ right.addEventListener("click", (event) => {
     if (event.target.classList.contains("add-to-cart-button")){
         const addPrice = event.target.getAttribute("data-price")
         const addName = event.target.getAttribute("data-name");
-    let newCartObject = {}
-        newCartObject.name = addName;
-        newCartObject.price = addPrice;
-        cart.push(newCartObject);
-        console.log(cart);
-  
-    //new cartObject
+        let newCartObject = {}
+            newCartObject.name = addName;
+            newCartObject.price = addPrice;
+            cart.push(newCartObject);
+            console.log(cart);
 
-    newTotal += parseInt(addPrice) / 100;
-    let taxTotal = .06 * newTotal;
-    let grandTotal = newTotal + taxTotal;
+        
+        // generate name box
+        let cartItemNameDisplay = document.createElement("p");
+        cartItemNameDisplay.innerText = addName;
+        cartNameHolder.appendChild(cartItemNameDisplay);
+        // generate quantity box
+        let cartQuantityContainer = document.querySelector(".cart-change-quantity-container");
+        let cartQuantity = document.querySelector(".cart-change-quantity");
+        let quantityBox = cartQuantity.cloneNode(true);
+        cartQuantityContainer.appendChild(quantityBox);
+
+        // generate price box
+        let cartPriceDisplay = document.createElement("p");
+        cartPriceDisplay.innerText = addPrice;
+        cartProductPriceHolder.appendChild(cartPriceDisplay);
+        
+
+        // add in if name === name, item.quantity++, else create new object
 
 
-    // grab selected item
-        console.log(subTotal.innerText);
-        subTotal.innerText = `Subtotal: ${newTotal}`;
-        tax.innerText = `Tax: ${taxTotal}`;
-        total.innerText = `Total ${grandTotal}`;
+        newTotal += parseInt(addPrice) / 100;
+        let taxTotal = .06 * newTotal;
+        let grandTotal = newTotal + taxTotal;
+
+
+        // All totals display
+        subTotal.innerText = `Subtotal: $${newTotal}`;
+        tax.innerText = `Tax: $${taxTotal}`;
+        total.innerText = `Total $${grandTotal}`;
+        cartSubTotal.innerText = `Subtotal: $${newTotal}`;
+        cartTax.innerText = `Tax: $${taxTotal}`;
+        cartTotal.innerText = `Total $${grandTotal}`;
 
     }
     // add slected item Name & price to cart
+    
 
 })
     // add slected item Name & price to cart
